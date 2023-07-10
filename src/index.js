17274077d18854b2bdcca884fb68a18f5f105838
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-07-10 16:22:13 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { store } from 'redux/store';
 import { Provider } from 'react-redux';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
+import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-// компонент Provider є обгорткою Redux
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,49 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contacts/contactsSlice';
 import { filterSlice } from './filter/filterSlice';
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = { key: 'contacts', storage };
+
+const persistedContactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
+
+export const persistor = persistStore(store);
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsSlice } from './contacts/contactsSlice';
+// import { filterSlice } from './filter/filterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsSlice.reducer,
+//     filter: filterSlice.reducer,
+//   },
+// });
